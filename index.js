8fecd7bd0b6a2ed761d49aa3474dad48e1f02b4e
Joseph Post <jp@jsphpst.com>
2016-01-13 13:13:22 -0600

    add CLI functionality

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
-
 'use strict'
+require('shelljs/global')
 
+var path = require('path')
+var argv = require('minimist')(process.argv.slice(2))
 var mysql = require('mysql')
 
 var database = process.env.MYSQL_DATABASE || 'jenkins'
@@ -9,13 +11,16 @@ var host = process.env.MYSQL_HOST || 'localhost'
 var port = process.env.MYSQL_PORT || '3306'
 var user = process.env.MYSQL_USER || 'jenkins'
 
-var conn = mysql.createConnection({
+var mysqlConfig = {
   database: database,
   host: host,
+  multipleStatements: true,
   password: process.env.MYSQL_PASSWORD,
   port: port,
   user: user
-})
+}
+
+var conn = mysql.createConnection(mysqlConfig)
 
 var environment = {
   BUILD_ID: process.env.BUILD_ID,
@@ -30,38 +35,81 @@ var environment = {
   JOB_NAME: process.env.JOB_NAME
 }
 
-var initDb = [
-  'DROP DATABASE IF EXISTS jenkins;',
-  'CREATE DATABASE jenkins;',
-  'USE jenkins;',
-  'CREATE TABLE IF NOT EXISTS build_history (build_history_id INT(11) NOT NULL AUTO_INCREMENT, BUILD_ID VARCHAR(100) DEFAULT NULL, BUILD_NUMBER VARCHAR(100) DEFAULT NULL, BUILD_URL VARCHAR(400) DEFAULT NULL, BUILD_TAG VARCHAR(100) DEFAULT NULL, DOCKER_REPO VARCHAR(100) DEFAULT NULL, DOCKER_TAG VARCHAR(100) DEFAULT NULL, GIT_BRANCH VARCHAR(100) DEFAULT NULL, GIT_COMMIT VARCHAR(100) DEFAULT NULL, GIT_URL VARCHAR(400) DEFAULT NULL, JOB_NAME VARCHAR(200) DEFAULT NULL, build_completed TIMESTAMP DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (build_history_id) ) ENGINE=InnoDB'
-]
-
-var log = function (text) {
-  console.log('[jenkins_persist]: ' + text)
+var insertSuccess = function (numRows, table) {
+  return 'Inserted ' + numRows + ' ' + rowOrRows(numRows) + ' into ' + table
 }
 
-if (!creds.password) {
-  console.log('ERR: Set password with MYSQL_PASSWORD environment variable.')
+var log = function (text) {
+  console.log('[jenkins-persist]: ' + text)
+}
+
+var rowOrRows = function (text) {
+  return text === 1 ? 'row' : 'rows'
+}
+
+var table = function (tableName) {
+  return mysqlConfig.database + '.' + tableName
+}
+
+var help = function () {
+  console.log('Usage:')
+  console.log('  jenkins-persist [options]')
+  console.log('')
+  console.log('Options:')
+  console.log('  --init      Initialize Database')
+  console.log('              [-f] for optional SQL file')
+  console.log('  --build     Write to the build table')
+  console.log('  --release   Write to the release table')
+  console.log('  -h,--help   Display this message')
+  console.log('')
+}
+
+if (!mysqlConfig.password) {
+  log('ERROR: Set password with MYSQL_PASSWORD environment variable.')
   process.exit()
 }
 
+/* ****************************************************************************
+ * MySql Queries
+ * ***************************************************************************/
 conn.connect()
 
-if (process.argv[2] === 'init') {
-  initDb.forEach(function (query) {
-    conn.query(query, function (err, result) {
-      log('result')
-    })
+if (argv.h || argv.help) {
+  help()
+} else if (argv.init) {
+  var scriptFile = argv.f ? argv.f : path.join(__dirname, 'create_db.sql')
+  var formattedScript = sed(/--.*/g, '', scriptFile)
+  log(scriptFile)
+  mysqlConfig['multipleStatements'] = true
+  var conn2 = mysql.createConnection(mysqlConfig)
+  conn2.query(formattedScript, function (err, result) {
+    if (err) {
+      log('ERROR: Problem with SQL statement at index ' + err.index)
+      throw err
+    }
   })
-} else {
-  conn.query('INSERT INTO build_history SET ?', environment, function (err, result) {
+  conn2.end()
+  log('initialized database')
+} else if (argv.release) {
+  conn.query('INSERT INTO ?? SET ?', [
+    table('release'),
+    environment
+  ], function (err, result) {
     if (err) throw err
-    var row = result.affectedRows === 1 ? 'row' : 'rows'
-    log('Inserted ' + result.affectedRows + ' ' + row + 
-        ' into ' + creds.database + '.build_history')
+    log(insertSuccess(result.affectedRows, table('release')))
+    log('result_id: ' + result.insertId)
+  })
+} else if (argv.build) {
+  conn.query('INSERT INTO ?? SET ?', [
+    table('build_history'),
+    environment
+  ], function (err, result) {
+    if (err) throw err
+    log(insertSuccess(result.affectedRows, table('build_history')))
     log('build_history_id: ' + result.insertId)
   })
+} else {
+  help()
 }
 
 conn.end()
