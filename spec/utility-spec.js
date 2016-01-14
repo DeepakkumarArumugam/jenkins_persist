7a96e22eba3107b41d47db7ff6071c672165e93f
Joseph Post <jp@jsphpst.com>
2016-01-14 15:10:15 -0600

    add tests

---
+++ spec/utility-spec.js
@@ -0,0 +1,29 @@
+'use strict'
+
+var hook = require('./../lib/hook-stdout')
+var u = require('./../lib/utility')
+
+describe('the log', function () {
+  it('should return a prepended string', function () {
+    var unhook = hook.setup(function (string, encoding, fd) {
+      return expect(string).toContain('[jenkins-persist]: test')
+    })
+    u.log('test')
+    unhook()
+  })
+})
+
+describe('the insert success message', function () {
+  it('should display "row" if first parameter is 1', function () {
+    var result = 'Inserted 1 row into test'
+    expect(u.insertSuccess(1, 'test')).toBe(result)
+  })
+  it('should display "rows" if first parameter is less than 1', function () {
+    var result = 'Inserted 0 rows into test'
+    expect(u.insertSuccess(0, 'test')).toBe(result)
+  })
+  it('should display "rows" if first parameter is greater than 1', function () {
+    var result = 'Inserted 2 rows into test'
+    expect(u.insertSuccess(2, 'test')).toBe(result)
+  })
+})
