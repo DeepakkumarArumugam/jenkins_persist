74f4101c921c8640a5471b9bd71974dfa8b72edc
Joseph Post <jp@jsphpst.com>
2016-01-15 09:51:13 -0600

    clean up index file

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 #!/usr/bin/env node
-
 'use strict'
 
 var argv = require('minimist')(process.argv.slice(2))
 var query = require('./lib/query')
-var u = require('./lib/utility')
 
 if (!process.env.MYSQL_PASSWORD) {
-  u.log('Error: Set password with MYSQL_PASSWORD environment variable.')
-  process.exit()
-}
-
-if (argv.init) {
+  console.log('Error: Set password with MYSQL_PASSWORD environment variable.')
+} else if (argv.init) {
   query.init(argv.f)
 } else if (argv.release) {
   query.release()
@@ -22,10 +17,8 @@ if (argv.init) {
   console.log('  jenkins-persist [options]')
   console.log('')
   console.log('Options:')
-  console.log('  --init      Initialize Database')
-  console.log('              [-f] for optional SQL file')
+  console.log('  --init      Initialize Database          [specify optional file with -f]')
   console.log('  --build     Write to the build table')
   console.log('  --release   Write to the release table')
-  console.log('  -h,--help   Display this message')
   console.log('')
 }
