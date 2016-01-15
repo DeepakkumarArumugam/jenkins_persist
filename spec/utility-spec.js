ee9046f3b25179512178cda04afbcd43c1db12ad
Joseph Post <jp@jsphpst.com>
2016-01-15 00:25:00 -0600

    add test

--- spec/utility-spec.js
+++ spec/utility-spec.js
@@ -3,16 +3,6 @@
 var hook = require('./../lib/hook-stdout')
 var u = require('./../lib/utility')
 
-describe('the log', function () {
-  it('should return a prepended string', function () {
-    var unhook = hook.setup(function (string, encoding, fd) {
-      return expect(string).toContain('[jenkins-persist]: test')
-    })
-    u.log('test')
-    unhook()
-  })
-})
-
 describe('the insert success message', function () {
   it('should display "row" if first parameter is 1', function () {
     var result = 'Inserted 1 row into test'
@@ -25,5 +15,15 @@ describe('the insert success message', function () {
   it('should display "rows" if first parameter is greater than 1', function () {
     var result = 'Inserted 2 rows into test'
     expect(u.insertSuccess(2, 'test')).toBe(result)
+  })
+})
+
+describe('the log', function () {
+  it('should return a prepended string', function () {
+    var unhook = hook.setup(function (string, encoding, fd) {
+      return expect(string).toContain('[jenkins-persist]: test')
+    })
+    u.log('test')
+    unhook()
   })
 })
