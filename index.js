e2f964c7b6db55b97e20b22d2ea44997f90c861b
Joseph Post <jp@jsphpst.com>
2016-01-19 12:35:52 -0600

    simplify index, colored output on error

--- index.js
+++ index.js
@@ -3,25 +3,25 @@
 
 var argv = require('minimist')(process.argv.slice(2))
 var query = require('./lib/query')
-var w = console.log
+var help = function () {
+  console.log('')
+  console.log('Usage:')
+  console.log('  jenkins-persist [options]')
+  console.log('')
+  console.log('Options:')
+  console.log('  --build     Write to the build table')
+  console.log('  --init      Initialize Database')
+  console.log('              (specify optional file with -f)')
+  console.log('  --release   Write to the release table')
+  console.log('')
+}
 
-if (!process.env.MYSQL_PASSWORD) {
-  w('Error: Set password with MYSQL_PASSWORD environment variable.')
-} else if (argv.init) {
+if (argv.init) {
   query.init(argv.f)
 } else if (argv.release) {
   query.release()
 } else if (argv.build) {
   query.build()
 } else {
-  w('Usage:')
-  w('  jenkins-persist [options]')
-  w('')
-  w('Options:')
-  w('  --build     Write to the build table')
-  w('  --init      Initialize Database')
-  w('              (specify optional file with -f)')
-  w('  --release   Write to the release table')
-  w('')
+  help()
 }
-
